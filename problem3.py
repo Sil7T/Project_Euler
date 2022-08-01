adfae0fb9160a70c0674e04f4314c62656f14fa9
唐思源 <tangsiyuan@tangsiyuandembp.mynetworksettings.com>
2022-08-01 10:35:41 -0400

    problem 3

--- problem3.py
+++ problem3.py
@@ -1,3 +1,18 @@
+# Final 
+
+def f(n):
+    factor = 2
+    while n>1: 
+        if n%factor==0:
+            yield factor
+            n=n/factor
+        else:
+            factor += 1
+
+primes = f(600851475143)
+for item in primes:
+    print(f'{item} is a prime factor')
+
 '''
 First Attempt
 Generate prime numbers
@@ -31,12 +46,12 @@ for number in primes:
 print(largestprimefactor)
 '''
 
-# Second Attempt
-
+# Second Attempt, works for particular question but is not a general solution
+'''
 def largestprimefactor():
     x = 3
     originalnumber = 600851475143
-    while x**2 < originalnumber:
+    while x**2 <= originalnumber:
         if originalnumber%2 == 0:
             originalnumber /= 2
         else:
@@ -49,3 +64,20 @@ def largestprimefactor():
         return originalnumber
 
 print(largestprimefactor())
+
+'''
+
+
+# Third Attempt, a little better but still not able to solve all instances of n
+
+'''
+def Euler3(n=16):
+    for i in range(2,int(n**(1/2))+1):
+        while n % i == 0:
+            n //= i
+            print(f"Yay, {i} is a factor, now we should test {n}")
+            if n == 1 or n == i:
+                return i
+
+print(Euler3())
+'''
